f09a56e3ecd5a833c64fd1f755c8defe30428db7
Nino Hasl <71927070+RainbowTensor@users.noreply.github.com>
2024-03-20 08:49:15 +0100

    Develop (#3)
    * latest version and code sniff fixes
    
    * latest version and code sniff fixes
    
    * Modal adjustments (#1)
    
    * latest version and code sniff and modal fixes
    
    * fixing missing property of response
    
    * codesniff fixes and js fix
    
    * adding tabs logic to module
    
    * adding tabs logic to module
    
    * adding tabs logic to module
    
    * fixes
    
    * namespace change (#2)
    
    * fixes
    
    * namespace change

--- src/BladeFx/Zed/Reports/assets/Zed/js/reports.entry.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-require('../sass/reports.scss')
-import CategorySwitcher from "./modules/category-switcher";
-
-const categorySwitcher = new CategorySwitcher();
-
-categorySwitcher.init();
