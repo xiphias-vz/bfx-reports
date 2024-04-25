8e3a54c78e20bbf7c4359270c0ba121a96f10d1d
Nino Hasl <71927070+RainbowTensor@users.noreply.github.com>
2024-04-25 11:46:25 +0200

    customer attribute fix (#4)

--- src/Xiphias/Zed/Reports/Communication/Formatter/ParameterFormatter.php
+++ src/Xiphias/Zed/Reports/Communication/Formatter/ParameterFormatter.php
@@ -7,6 +7,7 @@
 
 namespace Xiphias\Zed\Reports\Communication\Formatter;
 
+use Spryker\Shared\Customer\CustomerConstants;
 use Spryker\Zed\Sales\SalesConfig;
 use Symfony\Component\HttpFoundation\Request;
 use Xiphias\Shared\Reports\ReportsConstants;
@@ -21,9 +22,51 @@ class ParameterFormatter implements ParameterFormatterInterface
     public function formatRequestParameters(Request $request): array
     {
         return [
-            ReportsConstants::ATTRIBUTE => ReportsConstants::BLADE_FX_ORDER_ATTRIBUTE,
-            ReportsConstants::PARAMETER_NAME => ReportsConstants::BLADE_FX_ORDER_PARAM_NAME,
-            ReportsConstants::PARAMETER_VALUE => (int)$request->query->getInt(SalesConfig::PARAM_ID_SALES_ORDER),
+            ReportsConstants::ATTRIBUTE => $this->getAttributeValue($request),
+            ReportsConstants::PARAMETER_NAME => $this->getParamName($request),
+            ReportsConstants::PARAMETER_VALUE => $this->getParamValue($request),
         ];
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return string
+     */
+    protected function getAttributeValue(Request $request): string
+    {
+        if ($request->query->has(SalesConfig::PARAM_ID_SALES_ORDER)) {
+            return ReportsConstants::BLADE_FX_ORDER_ATTRIBUTE;
+        }
+
+        return ReportsConstants::BLADE_FX_CUSTOMER_ATTRIBUTE;
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return string
+     */
+    protected function getParamName(Request $request): string
+    {
+        if ($request->query->has(SalesConfig::PARAM_ID_SALES_ORDER)) {
+            return ReportsConstants::BLADE_FX_ORDER_PARAM_NAME;
+        }
+
+        return ReportsConstants::BLADE_FX_CUSTOMER_PARAM_NAME;
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return int
+     */
+    protected function getParamValue(Request $request): int
+    {
+        if ($request->query->has(SalesConfig::PARAM_ID_SALES_ORDER)) {
+            return (int)$request->query->getInt(SalesConfig::PARAM_ID_SALES_ORDER);
+        }
+
+        return (int)$request->query->getInt(CustomerConstants::PARAM_ID_CUSTOMER);
     }
 }
