da762a917f74d2db22c25df790235eeb11ad7435
javornovosel <javor.novosel@gmail.com>
2025-04-30 08:28:24 +0200

    SBFX-67 added modal

---
+++ src/Xiphias/Zed/SprykerBladeFxUser/assets/Zed/js/spryker-bladefx-user.entry.js
@@ -0,0 +1,9 @@
+'use strict';
+
+require('../sass/spryker-bladefx-user.scss')
+
+import ModalCreator from './modules/bladefx-user-requirement-information-modal-creator.js';
+
+const modalCreator = new ModalCreator();
+modalCreator.init();
+
