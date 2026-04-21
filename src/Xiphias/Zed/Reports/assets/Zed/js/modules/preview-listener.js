b510e6f3323ccf00e03d32302286e61df07df16c
javornovosel <javor.novosel@gmail.com>
2026-04-21 10:26:00 +0200

    reworking preview logic to work with new gui

--- src/Xiphias/Zed/Reports/assets/Zed/js/modules/preview-listener.js
+++ src/Xiphias/Zed/Reports/assets/Zed/js/modules/preview-listener.js
@@ -27,27 +27,29 @@ export default class PreviewListener {
             const instance = this
             let tableContainer = this.getTableContainer();
             if (tableContainer) {
+                if (!$.fn.DataTable.isDataTable(tableContainer)) {
+                    return
+                }
+
                 this.bootstrapModalObject = createModal(this.$previewModal)
-                var $reportsTable = $(tableContainer.querySelector('table')).DataTable();
-                $reportsTable.on('draw', function () {
+                var $reportsTable = $(tableContainer).DataTable();
+                $reportsTable.on('draw', function() {
                     let tableRows = tableContainer.querySelectorAll('tr')
                     tableRows.forEach((row) => {
-                        if (row.classList.contains('odd') || row.classList.contains('even')) {
-                            let button = row.querySelector('a.btn-edit');
-                            if (!button) {
-                                button = row.querySelector('a.btn-preview');
-                            }
+                        let button = row.querySelector('a.btn-edit');
+                        if (!button) {
+                            button = row.querySelector('a.btn-preview');
+                        }
 
-                            if (button !== null) {
-                                button.addEventListener('click', async (e) => {
-                                    e.preventDefault();
-                                    await instance.handleReportPreview(button);
-                                });
+                        if (button !== null) {
+                            button.addEventListener('click', async (e) => {
+                                e.preventDefault();
+                                await instance.handleReportPreview(button);
+                            });
 
-                                row.addEventListener('dblclick', async () => {
-                                    await instance.handleReportPreview(button);
-                                });
-                            }
+                            row.addEventListener('dblclick', async () => {
+                                await instance.handleReportPreview(button);
+                            });
                         }
                     });
 
@@ -84,18 +86,18 @@ export default class PreviewListener {
 
     getTableContainer() {
         if (this.reportTab) {
-            return this.reportTab.querySelector('div.dataTables_scrollBody')
+            return this.reportTab.querySelector('.bfx-reports-table[id]')
         }
 
         if (this.reportBlock) {
-            return this.reportBlock.querySelector('div.dataTables_scrollBody')
+            return this.reportBlock.querySelector('.bfx-reports-table[id]')
         }
 
-        return document.querySelector('div.dataTables_scrollBody')
+        return document.querySelector('.bfx-reports-table[id]')
     }
 
     goToFirstPageIfCurrentPageEmpty(tableContainer, $reportsTable) {
-        if (tableContainer.querySelector('.dataTables_empty')) {
+        if (tableContainer.querySelector('.dt-empty') || tableContainer.querySelector('.dataTables_empty')) {
             $reportsTable.page('first').draw('false');
         }
     }
